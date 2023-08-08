04906c5e7cce486b73af05d7965876f0987019a2
Vinoth Kannan <svkn.87@gmail.com>
2023-08-08 23:46:53 +0530

    PERF: add indexes on field option table columns `field_id` and `jira_id`.
    Querying the `field_options` table affects performance when there are lots of fields and options available.

---
+++ db/migrate/20230808054547_add_field_option_indexes.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class AddFieldOptionIndexes < ActiveRecord::Migration[7.0]
+  def change
+    add_index :jira_field_options,
+              :field_id,
+              name: :index_jira_field_options_on_field_id
+    add_index :jira_field_options,
+              :jira_id,
+              name: :index_jira_field_options_on_jira_id
+    add_index :jira_field_options,
+              %i[field_id jira_id],
+              unique: true,
+              name: :index_jira_field_options_on_field_id_and_jira_id
+  end
+end
