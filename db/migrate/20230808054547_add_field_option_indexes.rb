7b713c63c2c864dffc7db260decc99b04e008474
Vinoth Kannan <svkn.87@gmail.com>
2023-08-08 23:53:09 +0530

    Make stree happy

--- db/migrate/20230808054547_add_field_option_indexes.rb
+++ db/migrate/20230808054547_add_field_option_indexes.rb
@@ -2,12 +2,8 @@
 
 class AddFieldOptionIndexes < ActiveRecord::Migration[7.0]
   def change
-    add_index :jira_field_options,
-              :field_id,
-              name: :index_jira_field_options_on_field_id
-    add_index :jira_field_options,
-              :jira_id,
-              name: :index_jira_field_options_on_jira_id
+    add_index :jira_field_options, :field_id, name: :index_jira_field_options_on_field_id
+    add_index :jira_field_options, :jira_id, name: :index_jira_field_options_on_jira_id
     add_index :jira_field_options,
               %i[field_id jira_id],
               unique: true,
