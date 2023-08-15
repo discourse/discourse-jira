d9fd05be0d2eef74e4441ca571391b488a89106c
Vinoth Kannan <svkn.87@gmail.com>
2023-08-15 17:31:19 +0530

    DEV: execute the `sync_jira` job only when the Jira URL is present. (#33)
    Previously, it created the logs noise with `both URI are relative` exception when the plugin is enabled but the Jira URL is not set.

---
+++ spec/jobs/scheduled/sync_jira_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Jobs::SyncJira do
+  let(:job) { described_class.new }
+  subject(:execute) { job.execute({}) }
+
+  before do
+    SiteSetting.discourse_jira_enabled = true
+    SiteSetting.discourse_jira_url = "https://jira.example.com"
+  end
+
+  it "syncs issue types and projects" do
+    ::DiscourseJira::IssueType.expects(:sync!).once
+    ::DiscourseJira::Project.expects(:sync!).once
+    execute
+  end
+
+  it "does not sync if jira URL is blank" do
+    SiteSetting.discourse_jira_url = ""
+    ::DiscourseJira::IssueType.expects(:sync!).never
+    ::DiscourseJira::Project.expects(:sync!).never
+    execute
+  end
+end
