da50543f64b8ec891479254b6325116425bfc135
OsamaSayegh <asooomaasoooma90@gmail.com>
2025-02-21 12:06:12 +0300

    FIX: import i18n helper correctly

---
+++ spec/system/custom_category_settings_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+RSpec.describe "Jira custom category settings'", type: :system do
+  fab!(:current_user) { Fabricate(:admin) }
+
+  before do
+    SiteSetting.discourse_jira_enabled = true
+    sign_in(current_user)
+  end
+
+  it "renders successfully on the category settings section" do
+    visit("/new-category")
+    find(".edit-category-settings").click
+
+    expect(page).to have_css(
+      ".jira-project",
+      text: I18n.t("js.discourse_jira.category_setting.project"),
+    )
+    expect(page).to have_css(
+      ".jira-project",
+      text: I18n.t("js.discourse_jira.category_setting.issue_type"),
+    )
+    expect(page).to have_css(
+      ".jira-project",
+      text: I18n.t("js.discourse_jira.category_setting.num_likes_auto_create_issue"),
+    )
+  end
+end
